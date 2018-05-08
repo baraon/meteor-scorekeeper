1e5940cd079dd222a0b7ce8b7e6e5f4fc76b9e1e
baraon <gallegosbaraon@yahoo.com>
2018-05-08 16:55:33 -0500

    Initial commit

---
+++ imports/math.js
@@ -0,0 +1,7 @@
+export let add = function(a, b) {
+  return a + b;
+};
+
+export default function(a, b) {
+  return a+b;
+};
---
+++ imports/utils.js
@@ -0,0 +1,10 @@
+console.log('Utils file executed');
+
+export let greetUser = function () {
+  return 'hola user';
+};
+
+export let name = 'Baraon Gallegos';
+
+let myDefault = 'a diff default value';
+export default myDefault;
