41c56b570442f8e68974deb57cb2732a43ad7d47
baraon <gallegosbaraon@yahoo.com>
2018-05-09 09:38:30 -0500

    added render players function to client side js

--- client/main.js
+++ client/main.js
@@ -2,8 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+const players = [{
+  _id: '1',
+  name: 'Baraon',
+  score: 99
+}, {
+  _id: '2',
+  name: 'Erin',
+  score: 99
+}, {
+  _id: '3',
+  name: 'Ben',
+  score: 98
+}];
+
+const renderPlayers = function (playersList) {
+  return playersList.map(function (player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+  });
+};
+
 Meteor.startup(function () {
+  let title = 'Score keep';
   let name = 'Baraon'
-  let jsx = <p>Hello {name}</p>;
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}</p>
+      <p>This is another p.</p>
+      {renderPlayers(players)}
+    </div>
+  );
   ReactDOM.render(jsx, document.getElementById('app'));
 });
